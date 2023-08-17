3ebbf4e0cb61957e6303d49da10b26e6265e034a
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-08-17 22:31:44 +0100

    created temperature container, fetched and displayed weather data will work on the forecasting later

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState } from "react";
+import { ICONS_URLS } from "./icons";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchQuery, setSearchQuery] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [img, setImg] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  const getWeatherData = async () => {
+    const API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+    const URL = "https://api.openweathermap.org/data/2.5/weather";
+    const FULL_URL = `${URL}?q=${searchQuery}&appid=${API_KEY}&units=imperial`;
+
+    try {
+      if (searchQuery !== "") {
+        const response = await axios.get(FULL_URL);
+        setLoading(true);
+        if (response.status === 200) {
+          setLoading(false);
+          setWeatherData(response.data);
+          setErrorMessage("");
+          return response.data;
+        }
+      }
+    } catch (error) {
+      setLoading(false);
+      if (error.response && error.response.status === 404) {
+        setWeatherData(null);
+        setErrorMessage("Weather data not available for this location.");
+      } else {
+        setWeatherData(null);
+        setErrorMessage("An error occurred while fetching weather data.");
+        console.error(error);
+      }
+    }
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery(e.target.value);
+  };
+
+  const handlekeyDown = (e) => {
+    if (e.code === "Enter") {
+      e.preventDefault();
+      getWeatherData();
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="home">
+      <nav className="nav">
+        <div className="container nav__container">
+          <input
+            type="text"
+            value={searchQuery}
+            placeholder="Search for city"
+            onChange={handleSearch}
+            onKeyDown={handlekeyDown}
+          />
+          <button onClick={getWeatherData}>
+            {loading ? "Loading" : "Search"}
+          </button>
+          {loading && <h1>Loading...</h1>}
+          {errorMessage && <h1>{errorMessage}</h1>}
+        </div>
+      </nav>
+      <>
+        {weatherData && (
+          <section className="weather__section container">
+            <div>
+              <div>
+                <h1>{weatherData?.name}</h1>
+                <span>{weatherData?.weather[0]?.description}</span>
+              </div>
+              <img
+                src={ICONS_URLS[weatherData?.weather[0]?.icon]}
+                alt="Weather description"
+              />
+            </div>
+            <div>
+              <h1>{weatherData?.main?.temp}°C</h1>
+              <div>
+                <h2>Details</h2>
+                <article>
+                  <p>Feels like</p>
+                  <small>{weatherData?.main?.feels_like}°C</small>
+                </article>
+                <article>
+                  <p>Humidity</p>
+                  <small>{weatherData?.main?.humidity}°C</small>
+                </article>
+                <article>
+                  <p>Pressure</p>
+                  <small>{weatherData?.main?.pressure}°C</small>
+                </article>
+              </div>
+            </div>
+          </section>
+        )}
+      </>
+    </main>
+  );
 }
 
-export default App
+export default App;
